911d1e937fcd41c404c08467911f0cceb23abdb0
Aayush <aayushdhand@gmail.com>
2021-11-12 08:56:28 +0530

    initial commit

---
+++ footer.js
@@ -0,0 +1,53 @@
+document.write(`
+<footer id="footer">
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-4 col-md-6 footer-info">
+                        <img src="../assets/img/NRS-LOGO.webp" alt="NRS logo" style="height: 120px; width: 120px; margin-left: 35%;margin-right: 20%; margin-top: -20px;">
+                        <p style="font-size: 20px; margin-top: -10px; font-family: 'Fira Sans', sans-serif;" class="text-justify">National Retail Summit is a national level flagship event organized by MBA Retail Management Program first-year students at K J Somaiya Institute of Management.</p>
+                    </div>
+
+                    <div class="col-lg-4 col-md-12 footer-links">
+                        <h4>Useful Links</h4>
+                        <ul>
+                            <p></p>
+                            <li><i class="fa fa-angle-right"></i> <a href="../index.html#intro">Home</a></li>
+                            <li><i class="fa fa-angle-right"></i> <a href="../index.html#about">About us</a></li>
+                            <li><i class="fa fa-angle-right"></i> <a href="../index.html#events">Events</a></li>
+                            <li><i class="fa fa-angle-right"></i> <a href="../index.html#speakers">Guests</a></li>
+                            <li><i class="fa fa-angle-right"></i> <a href="../index.html#schedule">Schedule</a></li>
+                            </p>
+                        </ul>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 footer-contact" style="font-size:15px;">
+                        <h4>Contact Us</h4>
+                        <p style="color: #FFFFFF;">
+                            K J Somaiya Institute of Management, <br> Vidhyanagar, Vidyavihar,
+                            <br> Mumbai-400077. <br>
+                            <strong>Amjed Parvez Khan (Convener) |</strong><a href="tel: +91-8449968461">+91-8449968461</a><br>
+                            <strong>Kumar Devank (Co-Convener) |</strong><a href="tel: +91-9646422598">+91-9646422598</a><br>
+                            <strong>Email: </strong><a href="mailto: nrs.kjsim@somaiya.edu">nrs.kjsim@somaiya.edu</a><br>
+                        </p>
+                        <div class="social-links">
+                            <a href="https://www.facebook.com/NRS2K19/" class="facebook" target="_blank"><i class="fa fa-facebook"></i></a>
+                            <a href="https://instagram.com/nrs.simsr" class="instagram" target="_blank"><i class="fa fa-instagram"></i></a>
+                            <a href="https://www.linkedin.com/company/national-retail-summit" class="linkedin" target="_blank"><i class="fa fa-linkedin"></i></a>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="copyright">
+                &copy; Copyright <strong>NRS</strong>. All Rights Reserved
+            </div>
+        </div>
+    </footer>
+    <!-- #footer -->
+`);
---
+++ header.js
@@ -0,0 +1,32 @@
+document.write(`
+<header id="header">
+        <div class="container">
+
+            <div id="logo" class="pull-left">
+                <!-- Uncomment below if you prefer to use a text logo -->
+                <!-- <h1><a href="#main">C<span>o</span>nf</a></h1>-->
+                <a href="../index.html" class="scrollto"><img src="../assets/img/KJ-Logo.webp" alt="" title=""></a>
+            </div>
+
+            <nav id="nav-menu-container">
+                <ul class="nav-menu">
+                    <li class="menu-active"><a href="../index.html#intro">Home</a></li>
+                    <li><a href="../index.html#about">About</a></li>
+                    <li><a href="../index.html#events">Events</a></li>
+                    <li><a href="../index.html#speakers">Guests</a></li>
+                    <li><a href="../index.html#schedule">Schedule</a></li>
+                    <li><a href="../events/retailmela.html">Retail Mela</a></li>
+                    <li><a href="../index.html#gallery">Gallery</a></li>
+                    <div>
+                        <div id="nrslogo">
+                            <!-- Uncomment below if you prefer to use a text logo -->
+                            <!-- <h1><a href="#main">C<span>o</span>nf</a></h1>-->
+                            <a href="../index.html#intro" class="scrollto"><img src="../assets/img/NRS-LOGO.webp" alt="" title=""></a>
+                        </div>
+                </ul>
+
+            </nav>
+            <!-- #nav-menu-container -->
+            </div>
+    </header>
+`);
